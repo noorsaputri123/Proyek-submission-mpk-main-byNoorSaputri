75c86afc059be2f87ecb4d45bb8d868a11271cba
Ririe <nsaputri08@gmail.com>
2023-02-26 16:44:38 +0700

    perbaikan code pada latihan1 : todo 1

--- src/main/kotlin/com/dicoding/exam/latihan1/App.kt
+++ src/main/kotlin/com/dicoding/exam/latihan1/App.kt
@@ -17,9 +17,7 @@ package com.dicoding.exam.latihan1
  *  Sesuaikan fungsi di bawah ini dengan kode untuk mengecek apakah parameter number
  *  merupakan angka genap
  */
-fun isEvenNumber(number: Int) : Boolean {
-    return number % 2 == 0
-} //Editor by Noor Saputri
+fun isEvenNumber(number: Int) =  number % 2 == 0 //Editor by Noor Saputri
 
 /**
  *  TODO 2
